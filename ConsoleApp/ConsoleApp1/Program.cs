5d53bf70de9e5f41a61992dfe29bd1dc00686b66
salimovrashad <selimovresad33@gmail.com>
2023-10-10 21:28:11 +0400

    add task file

---
+++ ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+
+
+            for (int i = 0; i < array.Length/2; i++)
+            {
+                int arrayDigit = array[i];
+                array[i] = array[array.Length - 1 - i];
+                array[array.Length - 1 - i] = arrayDigit;
+            }
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+        }
+    }
+}
